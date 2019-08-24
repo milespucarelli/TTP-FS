db164449b7d861093dcbc44eccf3c8958c5da8b6
milespucarelli <miles.pucarelli@gmail.com>
2019-08-24 13:32:35 -0400

    Search and limited load working completely

--- ttp-front-end/src/components/Marketplace.js
+++ ttp-front-end/src/components/Marketplace.js
@@ -1,38 +1,66 @@
 import React, { Component } from 'react';
-import { Container, Card, Button } from 'semantic-ui-react'
+import { Container, Input, Card, Button } from 'semantic-ui-react'
 import Stock from './Stock'
 
 class Marketplace extends Component {
   state = {
     stocks: [],
+    filteredStocks: [],
     loadedStocks: [],
-    upperRange: 120
+    upperRange: 120,
+    searchTerm: ''
+  }
+
+  searchHandler = (e) => {
+    if (e.target.value) {
+      let filtered = this.state.stocks.filter(stock =>
+        stock.name.substring(0, e.target.value.length).toLowerCase() === e.target.value.toLowerCase()
+      )
+      this.setState({
+        searchTerm: e.target.value,
+        filteredStocks: filtered,
+        loadedStocks: filtered.slice(0, 120),
+        upperRange: 120
+      })
+    } else {
+      this.setState({
+        searchTerm: '',
+        filteredStocks: this.state.stocks,
+        loadedStocks: this.state.stocks.slice(0, 120),
+        upperRange: 120
+      })
+    }
   }
 
   clickHandler = () => {
     if (this.state.upperRange + 120 <= this.state.stocks.length) {
       this.setState({
-        loadedStocks: this.state.stocks.slice(0, this.state.upperRange + 120),
+        loadedStocks: this.state.filteredStocks.slice(0, this.state.upperRange + 120),
         upperRange: this.state.upperRange + 120
       })
     } else {
-      this.setState({loadedStocks: this.state.stocks.slice(0, this.state.stocks.length)})
+      this.setState({
+        loadedStocks: this.state.filteredStocks.slice(0, this.state.filteredStocks.length),
+        upperRange: this.state.filteredStocks.length
+      })
     }
   }
 
   componentDidMount() {
-    fetch('https://api.iextrading.com/1.0/ref-data/symbols')
+    fetch('https://cloud.iexapis.com/stable/ref-data/symbols?token=pk_3b067f6bb3e243388641ac4995837a75&filter=symbol,name')
       .then(res => res.json())
       .then(data => this.setState({stocks: data, loadedStocks: data.slice(0, 120)}))
   }
 
   render() {
+    let {searchTerm, upperRange, filteredStocks} = this.state
     return (
       <Container>
+        <Input size='massive' icon='search' placeholder='Search...' value={searchTerm} onChange={this.searchHandler}/>
         <Card.Group itemsPerRow={4}>
           {this.state.loadedStocks.map(stock => (<Stock {...stock} key={stock.symbol} />))}
         </Card.Group>
-        <Button color='green' onClick={this.clickHandler}>Load More</Button>
+        {upperRange < filteredStocks.length ? <Button color='green' onClick={this.clickHandler}>Load More</Button> : null}
       </Container>
     );
   }
--- ttp-front-end/src/components/Stock.js
+++ ttp-front-end/src/components/Stock.js
@@ -1,49 +1,66 @@
 import React, { Component } from 'react';
-import { Card, List, Button } from 'semantic-ui-react'
+import { Card, Container, List, Button } from 'semantic-ui-react'
 
 class Stock extends Component {
   state = {
-    sector: '',
-    securityType: '',
-    price: 0
+    price: 0,
+    industry: '',
+    website: '',
+    ceo: '',
+    sector: ''
   }
 
   componentDidMount() {
-    fetch(`https://api.iextrading.com/1.0/tops?symbols=${this.props.symbol}`)
+    fetch(`https://cloud.iexapis.com/stable/stock/${this.props.symbol}/company?token=pk_3b067f6bb3e243388641ac4995837a75&filter=industry,website,CEO,sector`)
       .then(res => res.json())
       .then(data => this.setState({
-        price: data[0].lastSalePrice,
-        sector: data[0].sector,
-        securityType: data[0].securityType
+        sector: data.sector,
+        industry: data.industry,
+        website: data.website,
+        ceo: data.CEO
       }))
+    fetch(`https://api.iextrading.com/1.0/tops/last?symbols=${this.props.symbol}`)
+      .then(res => res.json())
+      .then(data => {
+        if (data[0]) {
+          this.setState({ price: data[0].price})
+        }
+      })
   }
 
   render() {
     let {name, symbol} = this.props
+    let {price, industry, website, ceo, sector} = this.state
     return (
       <Card key={symbol}>
         <Card.Content>
-          <Card.Header>{name ? name : symbol}</Card.Header>
-          <Card.Meta>{symbol}</Card.Meta>
+          <Card.Header><a href={website}>{name ? name : symbol}</a></Card.Header>
+          <Card.Meta><h3>{symbol}</h3></Card.Meta>
           <Card.Description>
-            <List>
-              <List.Item>
-                <List.Header>Price</List.Header>
-                {this.state.price === 0 ? 'n/a' : this.state.price}
-              </List.Item>
-              <List.Item>
-                <List.Header>Sector</List.Header>
-                {this.state.sector}
-              </List.Item>
-              <List.Item>
-                <List.Header>Security Type</List.Header>
-                {this.state.securityType}
-              </List.Item>
-            </List>
+            <Container>
+              <List>
+                <List.Item>
+                  <List.Header>Price</List.Header>
+                  {price === 0 ? 'n/a' : price}
+                </List.Item>
+                <List.Item>
+                  <List.Header>Industry</List.Header>
+                  {industry ? industry : 'n/a'}
+                </List.Item>
+                <List.Item>
+                  <List.Header>Sector</List.Header>
+                  {sector ? sector : 'n/a'}
+                </List.Item>
+                <List.Item>
+                  <List.Header>CEO</List.Header>
+                  {ceo ? ceo : 'n/a'}
+                </List.Item>
+              </List>
+            </Container>
           </Card.Description>
         </Card.Content>
         <Card.Content extra>
-          <Button basic color={this.state.price === 0 ? 'red' :'green'}>Buy</Button>
+          <Button basic color={price === 0 ? 'red' :'green'}>Buy</Button>
         </Card.Content>
       </Card>
     );
